7052d983bd2f5c93a75f8434d38b9049272cd288
cliff <cliffmudigo5@gmail.com>
2023-09-11 21:02:31 +0300

    registration and login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 
 dotenv.config();
@@ -18,8 +19,9 @@ mongoose
     console.error("Error connecting to the database:", err);
   });
   app.use(express.json());
-
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is running on port 5000");
